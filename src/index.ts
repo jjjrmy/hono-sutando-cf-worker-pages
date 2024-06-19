0030b63d3f9beebe64932190001f61af4ec25a08
Jeremy Holstein <jjjrmy@gmail.com>
2024-06-19 13:55:54 -0400

    sutando endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { ModelNotFoundError, sutando } from 'sutando';
+import { User } from './models';
+import ClientD1 from 'knex-cloudflare-d1';
 
 type Bindings = {
-  [key in keyof CloudflareBindings]: CloudflareBindings[key]
+  DB: D1Database
 }
 
 const app = new Hono<{ Bindings: Bindings }>()
@@ -10,4 +13,35 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-export default app
+app.get('/users', async (c) => {
+  const users = await User.query().withCount({
+    posts: q => q.where('published', true)
+  }).get();
+  return c.json(users);
+});
+
+app.onError((err, c) => {
+  if (err instanceof ModelNotFoundError) {
+    return c.json({
+      message: err.message
+    }, 404);
+  }
+
+  return c.json({
+    message: 'Something went wrong'
+  }, 500);
+});
+
+export default {
+  fetch: (req: Request, env: Bindings) => {
+    sutando.addConnection({
+      client: ClientD1,
+      connection: {
+        database: env.DB
+      },
+      useNullAsDefault: true,
+    });
+
+    return app.fetch(req, env)
+  },
+}
